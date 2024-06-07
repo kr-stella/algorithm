3104d6b183d83329f173505473c8ff7a6932b2cb
Sumin <82007444+kr-st2lla@users.noreply.github.com>
2024-06-07 22:08:42 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.js
@@ -0,0 +1,7 @@
+function solution(a, b, c) {
+
+    if(a === b && b === c) return (a * 3) * (Math.pow(b, 2) * 3) * (Math.pow(c, 3) * 3);
+    if(a !== b && b !== c && c !== a) return a + b + c;
+    return (a + b + c) * (Math.pow(a, 2) + Math.pow(b, 2) + Math.pow(c, 2));
+    
+}
