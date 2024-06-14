e0b6485621a25f96bec2ebd4b177f4b28fab5a85
Sumin <82007444+kr-stella@users.noreply.github.com>
2024-06-15 00:06:37 +0900

    [level 0] Title: 문자열 묶기, Time: 6.86 ms, Memory: 56.1 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/0/181855. 문자열 묶기/문자열 묶기.js
@@ -0,0 +1,18 @@
+// const solution = (arr) => Math.max(...Object.values(arr.map(a => a.length).reduce((a, c) => ((a[c] = (a[c] || 0) + 1), a), {})));
+
+const solution = (arr) => {
+    
+    const cnt = {};
+    let res = 0;
+    for(const str of arr) {
+        
+        const length = str.length;
+        cnt[length] = (cnt[length] || 0) + 1;
+        if(cnt[length] > res)
+            res = cnt[length];
+        
+    }
+    
+    return res;
+    
+};
