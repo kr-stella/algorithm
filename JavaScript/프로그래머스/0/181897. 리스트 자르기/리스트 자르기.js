9fbf0ad3bb7efbbddb01512ec08be49d39d7bf97
Sumin <82007444+kr-stella@users.noreply.github.com>
2024-06-15 00:02:58 +0900

    Delete JavaScript/프로그래머스/0/181897. 리스트 자르기 directory

--- JavaScript/프로그래머스/0/181897. 리스트 자르기/리스트 자르기.js
+++
@@ -1,9 +0,0 @@
-function solution(n, slicer, arr) {
-    
-    const [ a, b, c ] = slicer;
-    if(n === 1) return arr.slice(0, b + 1);
-    if(n === 2) return arr.slice(a, arr.length);
-    if(n === 3) return arr.slice(a, b + 1);
-    if(n === 4) return arr.slice(a, b + 1).filter((_, i) => !(i % 2));
-    
-}
