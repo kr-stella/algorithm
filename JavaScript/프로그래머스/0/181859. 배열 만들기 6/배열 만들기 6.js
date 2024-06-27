a525599533bcc263aa954db2faf611309c555107
Sumin <82007444+kr-stella@users.noreply.github.com>
2024-06-27 18:15:56 +0900

    [level 0] Title: 배열 만들기 6, Time: 3.39 ms, Memory: 37.9 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.js
@@ -0,0 +1,17 @@
+const solution = arr => {
+    
+    let i = 0;
+    const res = [];
+    while(i < arr.length) {
+        if(!res.length || res[res.length - 1] !== arr[i]) {
+            res.push(arr[i]);
+            i++;
+        } else {
+            res.pop();
+            i++;
+        };
+    };
+    
+    return res.length? res:[-1];
+    
+}
