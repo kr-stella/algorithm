9cdf1b5fad49fd057e79b50c749b219b17919ff9
Sumin <82007444+kr-stella@users.noreply.github.com>
2024-09-07 00:36:06 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.26 ms, Memory: 33.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ JavaScript/프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -1,9 +1,16 @@
-const zerofill = (v, n) => `${`0`.repeat(n - v.length)}${v}`;
-const solution = (n, arr1, arr2) => {
+// const zerofill = (v, n) => `${`0`.repeat(n - v.length)}${v}`;
+// const solution = (n, arr1, arr2) => {
     
-    const data1 = [...arr1].map(a => a.toString(2)).map(v => v.length === n?v:zerofill(v, n));
-    const data2 = [...arr2].map(a => a.toString(2)).map(v => v.length === n?v:zerofill(v, n));
+//     const data1 = [...arr1].map(a => a.toString(2)).map(v => v.length === n?v:zerofill(v, n));
+//     const data2 = [...arr2].map(a => a.toString(2)).map(v => v.length === n?v:zerofill(v, n));
     
-    return data1.map((v, i) => [...v].map((a, j) => (+a || +[...data2[i]][j])? `#`:` `).join(``));
+//     return data1.map((v, i) => [...v].map((a, j) => (+a || +[...data2[i]][j])? `#`:` `).join(``));
     
-}
+// }
+
+
+
+
+const zerofill = (n, v) => `0`.repeat(n - v.length) + v;
+const solution = (n, arr1, arr2) => 
+    arr1.map((v, i) => zerofill(n, (v | arr2[i]).toString(2)).replace(/1|0/g, a => +a ? `#`:` `));
