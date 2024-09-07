a974429870e21d082dabad0d25f3d3ebe4287c22
Sumin <82007444+kr-stella@users.noreply.github.com>
2024-09-07 21:15:32 +0900

    [level 1] Title: 2016년, Time: 7.93 ms, Memory: 33.9 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/1/12901. 2016년/2016년.js
@@ -0,0 +1,8 @@
+const solution = (a, b) => {
+    
+    const date = new Date(`${2016}-${a}-${b}`);
+    const days = [`SUN`, `MON`, `TUE`, `WED`, `THU`, `FRI`, `SAT`];
+    
+    return days[date.getDay()];
+    
+}
