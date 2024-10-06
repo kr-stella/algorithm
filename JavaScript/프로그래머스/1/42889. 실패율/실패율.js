1b4de6f9e09f6aa771d4eed88e2e75d90edecdc4
Sumin <82007444+kr-stella@users.noreply.github.com>
2024-10-06 14:40:06 +0900

    [level 1] Title: 실패율, Time: 8.01 ms, Memory: 39.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/42889. 실패율/실패율.js
+++ JavaScript/프로그래머스/1/42889. 실패율/실패율.js
@@ -1,41 +1,32 @@
 const solution = (v, arr) => {
     
-    // 각 스테이지에 있는 사용자 수
-	const usersAtStage = Array(v + 1).fill(0);
-    // 각 스테이지에 도달한 총 사용자 수
-	const usersReached = Array(v + 1).fill(0);
-
-	// 각 사용자가 멈춰 있는 스테이지를 카운트
-	arr.forEach(stage => {
-		if(stage <= v)
-			usersAtStage[stage]++;
-	});
+    let users = arr.length;
     
-	// 누적 사용자 수 계산
-	let total = arr.length;
-	for(let i = 1; i <= v; i++) {
-		usersReached[i] = total;
-		total -= usersAtStage[i];
-	}
+    /** 스테이지 도착 */
+    const arrived = new Array(v + 1).fill(0);
+    arr.forEach(a => {
+        arrived[a] = (arrived[a] || 0) + 1;
+    });
     
-    // console.log(usersAtStage)
-    // console.log(usersReached)
+    /** 각 스테이지 별 실패율 */
+    const rates = [];
+    for(let stage = 1; stage <= v; stage++) {
+        
+        const rate = arrived[stage] / users;
+        rates.push({ stage, rate });
+        
+        /** 총 유저를 줄여줌 */
+        users -= arrived[stage];
 
-	// 각 스테이지의 실패율 계산
-	let rates = [];
-	for(let i = 1; i <= v; i++) {
-		let rate = usersReached[i] > 0? usersAtStage[i] / usersReached[i] : 0;
-		rates.push({ stage: i, rate });
-	}
-
-	// 실패율에 따라 내림차순으로 정렬 ( 실패율이 같다면 스테이지 번호가 작은 것이 먼저 )
-	rates.sort((a, b) => {
-		if (a.rate === b.rate) return a.stage - b.stage;
-		return b.rate - a.rate;
-	});
-
-	// 정렬된 스테이지 번호만 추출
-	return rates.map(stage => stage.stage);
+    }
+    
+    /** 실패율이 같은경우 스테이지별로 오름차순, 아닌 경우 실패율 내림차순( 높은 순서 ) 정렬 */
+    rates.sort((a, b) => {
+        if(a.rate == b.rate) return a.stage - b.stage;
+        else return b.rate - a.rate;
+    });
+    
+    return rates.map(a => a.stage);
     
 }
 
