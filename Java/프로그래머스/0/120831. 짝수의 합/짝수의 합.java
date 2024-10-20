3cf23771cc94c7bc1fe6c96f75323b83f8e8087b
Sumin <82007444+kr-stella@users.noreply.github.com>
2024-10-20 18:57:12 +0900

    [level 0] Title: 짝수의 합, Time: 2.31 ms, Memory: 77.4 MB -BaekjoonHub

--- Java/프로그래머스/0/120831. 짝수의 합/짝수의 합.java
+++ Java/프로그래머스/0/120831. 짝수의 합/짝수의 합.java
@@ -2,6 +2,10 @@ import java.util.stream.IntStream;
 
 class Solution {
     public int solution(int n) {
-        return IntStream.rangeClosed(0, n).filter(a -> a % 2 == 0).sum();
+        
+        return IntStream.iterate(2, i -> i <= n, i -> i + 2).sum();
+        /** 0에서 n까지 만들고 나머지 비교해서 필터링 한 후 sum = 비효율 */
+        // return IntStream.rangeClosed(0, n).filter(a -> a % 2 == 0).sum();
+        // for(int i=2; i<=n; i+=2){ answer += i; } return answer;
     }
 }
