71eadda98a4904454611887197a15bb0b5ec9ad4
Sumin <82007444+kr-stella@users.noreply.github.com>
2024-10-24 10:40:20 +0900

    [level 1] Title: 둘만의 암호, Time: 0.26 ms, Memory: 33 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/155652. 둘만의 암호/둘만의 암호.js
+++ JavaScript/프로그래머스/1/155652. 둘만의 암호/둘만의 암호.js
@@ -1,5 +1,23 @@
 const solution = (s, skip, index) => {
     
+//     skip = [...skip].map(v => v.charCodeAt());
+//     return [...s].map(v => v.charCodeAt()).map(v => {
+        
+//         let len = 0;
+//         // true 일 때
+//         while(len !== index) {
+            
+//             v += 1;
+//             if(v > 122) v = 97;
+//             if(!skip.includes(v)) len++;
+            
+//         }
+        
+//         return String.fromCharCode(v);
+        
+//     }).join(``);
+    
+    
     const data = `abcdefghijklmnopqrstuvwxyz`.match(new RegExp(`[^${skip}]`, `g`), ``);
     return [...s].map(v => data[(data.indexOf(v) + index) % data.length]).join(``);
     
