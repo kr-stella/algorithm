e62528d9776fe08ffa70490cb3db837fd2ca9c76
Sumin <82007444+kr-stella@users.noreply.github.com>
2024-10-24 14:21:15 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 5.44 ms, Memory: 81.5 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int price) {
+        
+        if(price >= 500000)
+            return (int) Math.floor(price * 0.8); 
+
+        if(price >= 300000)
+            return (int) Math.floor(price * 0.9);
+
+        if(price >= 100000)
+            return (int) Math.floor(price * 0.95);
+
+        return (int) Math.floor(price);
+        
+    }
+}
