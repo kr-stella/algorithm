00633441fc944fb753ca1a1d0183fd54679747bd
Sumin <82007444+kr-stella@users.noreply.github.com>
2024-11-17 17:53:23 +0900

    [level 0] Title: n의 배수 고르기, Time: 0.03 ms, Memory: 78.8 MB -BaekjoonHub

--- Java/프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.java
+++ Java/프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.java
@@ -1,15 +1,33 @@
-import java.util.*;
-
 class Solution {
     public int[] solution(int n, int[] numlist) {
         
-        List<Integer> answer = new ArrayList<>();
+        int size = 0;
         for(int v : numlist) {
-            if(v % n == 0) 
-                answer.add(v);
+            if(v % n == 0)
+                size++;
         };
         
-        return answer.stream().mapToInt(x -> x).toArray();
+        int[] answer = new int[size];
+        int index = 0;
+        for(int v : numlist) {
+            if(v % n == 0) {
+                answer[index] = v;
+                index++;
+            };
+        }
+        
+        return answer;
+        
+//         List<Integer> answer = new ArrayList<>();
+//         for(int v : numlist) {
+//             if(v % n == 0) 
+//                 answer.add(v);
+//         };
+        
+//         return answer.stream().mapToInt(x -> x).toArray();
+//         return Arrays.stream(numlist)
+//                 .filter(i -> i % n == 0)
+//                 .toArray();
         
     }
 }
