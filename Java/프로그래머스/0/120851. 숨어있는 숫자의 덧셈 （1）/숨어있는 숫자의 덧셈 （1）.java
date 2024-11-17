c00ce9c8821c8092b321192f53158a993e55251d
Sumin <82007444+kr-stella@users.noreply.github.com>
2024-11-17 18:14:43 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.64 ms, Memory: 72.5 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
@@ -0,0 +1,17 @@
+import java.util.regex.*;
+
+class Solution {
+    public int solution(String my_string) {
+        
+        int answer = 0;
+        String[] str = my_string.split("\\d");
+        Pattern pattern = Pattern.compile("[0-9]{1}");  // 한자리 수
+        // Pattern pattern = Pattern.compile("[0-9]+");    // 연속된 수(123, 10)
+        Matcher matcher = pattern.matcher(my_string);
+        while(matcher.find())
+            answer += Integer.parseInt(matcher.group());
+        
+        return answer;
+        
+    }
+}
