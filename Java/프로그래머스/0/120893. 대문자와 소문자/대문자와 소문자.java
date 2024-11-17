9795e25ec61f169b9c58e474f555bfe453334896
Sumin <82007444+kr-stella@users.noreply.github.com>
2024-11-17 17:32:21 +0900

    [level 0] Title: 대문자와 소문자, Time: 4.83 ms, Memory: 84 MB -BaekjoonHub

--- Java/프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.java
+++ Java/프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.java
@@ -1,14 +1,24 @@
 class Solution {
     public String solution(String str) {
         
+//         String answer = "";
+//         for(String v : str.split("")) {
+            
+//             String upper = v.toUpperCase();
+//             String lower = v.toLowerCase();
+            
+//             if(v.equals(upper)) answer += lower;
+//             else answer += upper;
+            
+//         }
+        
         String answer = "";
-        for(String v : str.split("")) {
+        for(int i = 0; i < str.length(); i++){
             
-            String upper = v.toUpperCase();
-            String lower = v.toLowerCase();
-            
-            if(v.equals(upper)) answer += lower;
-            else answer += upper;
+            char c = str.charAt(i);
+            if(Character.isUpperCase(c))
+                answer += String.valueOf(c).toLowerCase();
+            else answer += String.valueOf(c).toUpperCase();
             
         }
         
