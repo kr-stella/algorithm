a2463f94cf4cf70d04476360c0b53c51c0db12c6
Sumin <82007444+kr-stella@users.noreply.github.com>
2025-02-06 17:48:04 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.03 ms, Memory: 83.2 MB -BaekjoonHub

--- Java/프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
+++ Java/프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
@@ -1,11 +1,13 @@
 class Solution {
     public int solution(int slice, int n) {
         
-        int ahrt = n / slice;
-        int skajwl = n % slice;
+//         int ahrt = n / slice;
+//         int skajwl = n % slice;
         
-        if(skajwl == 0) return ahrt;
-        else return ahrt + 1;
+//         if(skajwl == 0) return ahrt;
+//         else return ahrt + 1;
+        
+        return n % slice == 0? n / slice : n / slice + 1;
         
     }
 }
