f4010e304e6377e09f5322d5ee840e108fffa889
Sumin <82007444+kr-stella@users.noreply.github.com>
2025-02-09 21:59:39 +0900

    [level 0] Title: 배열 자르기, Time: 0.02 ms, Memory: 84.5 MB -BaekjoonHub

--- Java/프로그래머스/0/120833. 배열 자르기/README.md
+++ Java/프로그래머스/0/120833. 배열 자르기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 배열 자르기 - 120833 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120833?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120833) 
 
 ### 성능 요약
 
-메모리: 76.8 MB, 시간: 3.73 ms
+메모리: 84.5 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 20일 19:48:17
+2025년 02월 09일 21:59:36
 
 ### 문제 설명
 
--- Java/프로그래머스/0/120833. 배열 자르기/배열 자르기.java
+++ Java/프로그래머스/0/120833. 배열 자르기/배열 자르기.java
@@ -2,6 +2,19 @@ import java.util.stream.*;
 
 class Solution {
     public int[] solution(int[] numbers, int num1, int num2) {
-        return IntStream.rangeClosed(num1, num2).map(a -> numbers[a]).toArray();
+        
+        // int cnt = 0;
+        int[] answer = new int[num2 - num1 + 1];
+        System.arraycopy(numbers, num1, answer, 0, num2 - num1 + 1);
+        return answer;
+        // return IntStream.rangeClosed(num1, num2).map(a -> numbers[a]).toArray();
+        // return Arrays.stream(numbers, num1, num2 + 1).toArray();
+//         for(int i = num1; i <= num2; i++) {
+//             answer[cnt] = numbers[i];
+//             cnt++;
+//         }
+            
+        
+//         return answer;
     }
 }
