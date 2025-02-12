20bcbc9a1008638f19d0fad42adc1d7a927eef98
Sumin <82007444+kr-stella@users.noreply.github.com>
2025-02-12 21:35:06 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 2.45 ms, Memory: 84.8 MB -BaekjoonHub

--- Java/프로그래머스/0/120825. 문자 반복 출력하기/README.md
+++ Java/프로그래머스/0/120825. 문자 반복 출력하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 문자 반복 출력하기 - 120825 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120825?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120825) 
 
 ### 성능 요약
 
-메모리: 79.7 MB, 시간: 1.72 ms
+메모리: 84.8 MB, 시간: 2.45 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 09일 22:01:46
+2025년 02월 12일 21:35:05
 
 ### 문제 설명
 
--- Java/프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.java
+++ Java/프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.java
@@ -1,11 +1,34 @@
+import java.util.*;
+import java.util.stream.*;
+
 class Solution {
-    public String solution(String my_string, int n) {
+    public String solution(String str, int n) {
         
-        String answer = "";
-        for(String str : my_string.split(""))
-            answer += str.repeat(n);
+//         String answer = "";
+//         for(String str : str.split(""))
+//             answer += str.repeat(n);
             
-        return answer;
+//         return answer;
+        
+        // String[] test = "12345".split("");
+        // List<Integer> test2 = Arrays.stream(test).mapToInt(Integer::parseInt)
+        //.boxed()
+        //.collect(Collectors.toList());
+        // int[] test2 = Arrays.stream(test).mapToInt(Integer::parseInt).toArray();
+        
+        // boxed = int를 Integer로 변환 / 이게 없다면 List<int> 가 되는데 이건 불가능함
+        
+        
+        
+        
+        // String[] arr = str.split("");
+        // List<String> arr2 = Arrays.asList(arr);
+        // return Arrays.stream(arr).map(a -> a.repeat(n)).collect(Collectors.joining());
+        // return arr2.stream().map(a -> a.repeat(n)).collect(Collectors.joining());
+        // return Arrays.stream(str.split("")).map(a -> a.repeat(n)).collect(Collectors.joining());
+        // return Arrays.stream(str.split("")).map(a -> a.repeat(n)).collect(Collectors.joining());
+        return Arrays.asList(str.split("")).stream().map(a -> a.repeat(n)).collect(Collectors.joining());
+        
         
     }
 }
