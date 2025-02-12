ce5609b0106bafc93ddaa449c4d43bc7eb9fb260
Sumin <82007444+kr-stella@users.noreply.github.com>
2025-02-12 21:55:40 +0900

    [level 0] Title: 배열의 유사도, Time: 1.11 ms, Memory: 85.7 MB -BaekjoonHub

--- Java/프로그래머스/0/120903. 배열의 유사도/배열의 유사도.java
+++ Java/프로그래머스/0/120903. 배열의 유사도/배열의 유사도.java
@@ -3,16 +3,23 @@ import java.util.*;
 class Solution {
     public int solution(String[] s1, String[] s2) {
         
-        int answer = 0;
-        Set<String> ref = new HashSet<>(Arrays.asList(s2));
-        for(String a : s1) {
-            if(ref.contains(a))
-                answer++;
-            // System.out.println(a);
-            // ㅁ
-        }
+        List<String> convert1 = Arrays.asList(s1);
+        // List<String> convert2 = Arrays.asList(s2);
+        /** 중복값 제거 */
+        Set<String> convert2 = new HashSet<>(Arrays.asList(s2));
+        //return (int) convert1.stream().filter(convert2::contains).count();
+        return (int) Arrays.stream(s1).filter(convert2::contains).count();
         
-        return answer;
+//         int answer = 0;
+//         Set<String> ref = new HashSet<>(Arrays.asList(s2));
+//         for(String a : s1) {
+//             if(ref.contains(a))
+//                 answer++;
+//             // System.out.println(a);
+//             // ㅁ
+//         }
+        
+//         return answer;
         
         
     }
