83b0e7c90f2d0ca28d896684d020edf7806f2726
Sumin <82007444+kr-stella@users.noreply.github.com>
2025-02-12 18:32:10 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 2.75 ms, Memory: 78.4 MB -BaekjoonHub

--- Java/프로그래머스/0/120585. 머쓱이보다 키 큰 사람/README.md
+++ Java/프로그래머스/0/120585. 머쓱이보다 키 큰 사람/README.md
@@ -1,10 +1,10 @@
 # [level 0] 머쓱이보다 키 큰 사람 - 120585 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120585?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120585) 
 
 ### 성능 요약
 
-메모리: 76.4 MB, 시간: 0.03 ms
+메모리: 78.4 MB, 시간: 2.75 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 20일 20:49:09
+2025년 02월 12일 18:32:08
 
 ### 문제 설명
 
--- Java/프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.java
+++ Java/프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.java
@@ -1,10 +1,13 @@
+import java.util.stream.*;
+
 class Solution {
     public int solution(int[] array, int height) {
-        int result = 0;
-        for(int h:array) {
-            if(h > height)
-                result++;
-        }
-        return result;
+        // int result = 0;
+        // for(int h:array) {
+        //     if(h > height)
+        //         result++;
+        // }
+        // return result;
+        return (int) IntStream.of(array).filter(a -> a > height).count();
     }
 }
