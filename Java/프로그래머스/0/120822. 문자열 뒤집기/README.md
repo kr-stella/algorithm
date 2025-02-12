65571a8646ead7715f2ecdbedea23b61e3312a40
Sumin <82007444+kr-stella@users.noreply.github.com>
2025-02-12 20:52:07 +0900

    [level 0] Title: 문자열 뒤집기, Time: 1.80 ms, Memory: 83.3 MB -BaekjoonHub

--- Java/프로그래머스/0/120822. 문자열 뒤집기/README.md
+++ Java/프로그래머스/0/120822. 문자열 뒤집기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 문자열 뒤집기 - 120822 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120822?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120822) 
 
 ### 성능 요약
 
-메모리: 76.2 MB, 시간: 3.35 ms
+메모리: 83.3 MB, 시간: 1.80 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 20일 21:05:50
+2025년 02월 12일 20:52:04
 
 ### 문제 설명
 
--- Java/프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.java
+++ Java/프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -3,10 +3,26 @@ import java.util.stream.*;
 class Solution {
     public String solution(String str) {
         
-        return IntStream.range(0, str.length())
-                    .mapToObj(i -> str.charAt(str.length() - i - 1))
-                    .map(Object::toString)
-                    .collect(Collectors.joining());
+        int cnt = 0;
+        int len = str.length() - 1;
+        String[] arr = str.split("");
+        String answer = "";
+        for(String s : arr) {
+            // System.out.println(arr[len - cnt]);
+            // System.out.println(s);
+            answer += arr[len - cnt];
+            cnt++;
+        }
+        
+        return answer;
+            
+        
+        // return IntStream.range(0, str.length())
+        //             .mapToObj(i -> str.charAt(str.length() - i - 1))
+        //             .map(Object::toString)
+        //             .collect(Collectors.joining());
+        
+        
         // return String.valueOf(IntStream.range(0, str.split("").length).mapToObj(a -> str.split("")[str.split("").length - 1 - a]).collect(Collectors.joining()));
     }
 }
