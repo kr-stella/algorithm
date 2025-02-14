5adbe7ad317edf38ebddf0bd4d264313ecc8ffd9
Sumin <82007444+kr-stella@users.noreply.github.com>
2025-02-14 09:20:55 +0900

    [level 0] Title: 짝수는 싫어요, Time: 7.58 ms, Memory: 88.9 MB -BaekjoonHub

--- Java/프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
+++ Java/프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -4,11 +4,14 @@ import java.util.stream.*;
 class Solution {
     public int[] solution(int n) {
         
-        int[] result = new int[(n + 1) / 2];
-        for(int i = 0; i < result.length; i++)
-            result[i] = i * 2 + 1;
+//         int[] result = new int[(n + 1) / 2];
+//         for(int i = 0; i < result.length; i++)
+//             result[i] = i * 2 + 1;
         
-        return result;
+//         return result;
+        
+        // IntStream.rangeClosed(1, (n + 1) / 2).map(a -> a * 2 - 1).forEach(System.out::println);
+        return IntStream.rangeClosed(1, (n + 1) / 2).map(a -> a * 2 - 1).toArray();
         
     }
 }
