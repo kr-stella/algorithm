65421d6d59634b26b93892d40c3725759dbe2da5
Sumin <82007444+kr-stella@users.noreply.github.com>
2025-02-16 11:11:21 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 1.92 ms, Memory: 81.5 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] numbers) {
+        
+        /** T1 */
+        // 오름차순 정렬 후 앞 2개, 뒤 2개 곱해서 비교
+        Arrays.sort(numbers);
+        // for(int a : numbers)
+        //     System.out.println(a);
+        // int a = numbers[0] * numbers[1];
+        // int b = numbers[numbers.length - 2] * numbers[numbers.length - 1];
+        // if(a > b) return a;
+        // else return b;
+        // return Math.max(a, b);
+        
+        return Math.max(numbers[0] * numbers[1], numbers[numbers.length - 2] * numbers[numbers.length - 1]);
+        
+    }
+}
