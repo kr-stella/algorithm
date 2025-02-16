922e71625b74ccf76dad1245932817f840f4a8f5
Sumin <82007444+kr-stella@users.noreply.github.com>
2025-02-16 19:35:23 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 2.80 ms, Memory: 76.9 MB -BaekjoonHub

--- Java/프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
+++ Java/프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
@@ -4,10 +4,17 @@ import java.util.stream.*;
 class Solution {
     public int solution(String myString, String pat) {
         
+        String convert = "";
+        for(String str : myString.split(""))
+            convert += "A".equals(str)? "B" : "A";
+        
+        return convert.contains(pat)? 1 : 0;
+        
+        /** T1 */
         // String convert = Arrays.stream(myString.split("")).map(a -> "A".equals(a)? "B":"A").collect(Collectors.joining());
         // return convert.contains(pat)? 1 : 0;
         
-        return Arrays.stream(myString.split("")).map(a -> "A".equals(a)? "B":"A").collect(Collectors.joining()).contains(pat)? 1 : 0;
+        // return Arrays.stream(myString.split("")).map(a -> "A".equals(a)? "B":"A").collect(Collectors.joining()).contains(pat)? 1 : 0;
         
     }
 }
