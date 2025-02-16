ccae41bee4e78f3d3c072869bdb463064987256e
Sumin <82007444+kr-stella@users.noreply.github.com>
2025-02-16 16:23:20 +0900

    [level 0] Title: 약수 구하기, Time: 3.64 ms, Memory: 86.3 MB -BaekjoonHub

--- Java/프로그래머스/0/120897. 약수 구하기/약수 구하기.java
+++ Java/프로그래머스/0/120897. 약수 구하기/약수 구하기.java
@@ -6,23 +6,38 @@ import java.util.Map.Entry;
 class Solution {
     public int[] solution(int n) {
         
-        List<Integer> list = new ArrayList<>();
+        Set<Integer> set = new HashSet<>();
         /** 본인들 */
-        // list.add(1); list.add(n); >> 이렇게 하면 n이 1일때가 안됨
+        set.add(1); set.add(n);
         for(int i = 1; i <= (int) Math.sqrt(n); i++) {
             if(n % i == 0) {
-                list.add(i);
-                int ahrt = (int) n / i;
-                // 이미 list에 포함돼있으면 추가하면 안됨( 25의 경우 5, 5 )
-                if(!list.contains(ahrt))
-                    list.add(ahrt);
+                set.add(i);
+                set.add((int) n / i);
             }
         }
         
-        int[] result = list.stream().mapToInt(Integer::intValue).toArray();
+        int[] result = set.stream().mapToInt(Integer::intValue).toArray();
         Arrays.sort(result);
-        // System.out.println(test);
+        
         return result;
+        
+//         List<Integer> list = new ArrayList<>();
+//         /** 본인들 */
+//         // list.add(1); list.add(n); >> 이렇게 하면 n이 1일때가 안됨
+//         for(int i = 1; i <= (int) Math.sqrt(n); i++) {
+//             if(n % i == 0) {
+//                 list.add(i);
+//                 int ahrt = (int) n / i;
+//                 // 이미 list에 포함돼있으면 추가하면 안됨( 25의 경우 5, 5 )
+//                 if(!list.contains(ahrt))
+//                     list.add(ahrt);
+//             }
+//         }
+        
+//         int[] result = list.stream().mapToInt(Integer::intValue).toArray();
+//         Arrays.sort(result);
+//         // System.out.println(test);
+//         return result;
         
 //         // int[] test = list.stream().mapToInt(Integer::intValue).toArray();
 //         // System.out.println(test);
