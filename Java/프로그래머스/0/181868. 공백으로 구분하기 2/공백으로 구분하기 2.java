fbcbbe2eafd70f163b28dc8fa1ee0745c4180b4e
Sumin <82007444+kr-stella@users.noreply.github.com>
2025-02-16 19:55:59 +0900

    [level 0] Title: 공백으로 구분하기 2, Time: 1.88 ms, Memory: 74.2 MB -BaekjoonHub

--- Java/프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
+++ Java/프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
@@ -4,17 +4,21 @@ import java.util.stream.*;
 class Solution {
     public String[] solution(String my_string) {
         
-        String[] answer = {};
-        List<String> result = new ArrayList<>();
-        for(String str : my_string.split(" "))
-            if(!str.equals(""))
-                result.add(str);
+        System.out.println(my_string.trim());
+        return my_string.trim().split("\\s+");
         
-        // List<Integer> s2 = Arrays.asList(1, 2, 3, 4, 5);
-        // int[] t2 = s2.stream().mapToInt(Integer::intValue).toArray();
+        /** T1 */
+//         String[] answer = {};
+//         List<String> result = new ArrayList<>();
+//         for(String str : my_string.split(" "))
+//             if(!str.equals(""))
+//                 result.add(str);
         
-        // return result.stream().map(a -> String.valueOf(a)).toArray();
-        return result.stream().toArray(String[]::new);
+//         // List<Integer> s2 = Arrays.asList(1, 2, 3, 4, 5);
+//         // int[] t2 = s2.stream().mapToInt(Integer::intValue).toArray();
+        
+//         // return result.stream().map(a -> String.valueOf(a)).toArray();
+//         return result.stream().toArray(String[]::new);
         
     }
 }
