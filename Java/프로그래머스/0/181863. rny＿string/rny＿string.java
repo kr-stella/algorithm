ac487c66583bce227560de82756d762b15b55202
Sumin <82007444+kr-stella@users.noreply.github.com>
2025-02-16 18:28:55 +0900

    [level 0] Title: rny_string, Time: 0.39 ms, Memory: 80.3 MB -BaekjoonHub

--- Java/프로그래머스/0/181863. rny＿string/rny＿string.java
+++ Java/프로그래머스/0/181863. rny＿string/rny＿string.java
@@ -1,5 +1,15 @@
+import java.util.regex.*;
+
 class Solution {
     public String solution(String str) {
+        
         return str.replaceAll("m", "rn");
+//         Pattern pattern = Pattern.compile("[m]");
+//         Matcher matcher = pattern.matcher(str);
+//         while(matcher.find())
+//             str = str.replace(matcher.group(), "rn");
+            
+//         return str;
+        
     }
 }
