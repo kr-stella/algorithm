fed45f3e1797c629e35d7fb13b0e04c8fdfac3d3
Sumin <82007444+kr-stella@users.noreply.github.com>
2025-02-16 19:53:29 +0900

    [level 0] Title: 공백으로 구분하기 2, Time: 10.64 ms, Memory: 85.2 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.util.stream.*;
+
+class Solution {
+    public String[] solution(String my_string) {
+        
+        String[] answer = {};
+        List<String> result = new ArrayList<>();
+        for(String str : my_string.split(" "))
+            if(!str.equals(""))
+                result.add(str);
+        
+        // List<Integer> s2 = Arrays.asList(1, 2, 3, 4, 5);
+        // int[] t2 = s2.stream().mapToInt(Integer::intValue).toArray();
+        
+        // return result.stream().map(a -> String.valueOf(a)).toArray();
+        return result.stream().peek(System.out::println).toArray(String[]::new);
+        
+    }
+}
