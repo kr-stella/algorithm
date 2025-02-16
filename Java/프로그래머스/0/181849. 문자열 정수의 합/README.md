73bd105ee3279963fce4333bd34e5f0dc9696ba2
Sumin <82007444+kr-stella@users.noreply.github.com>
2025-02-16 11:44:12 +0900

    [level 0] Title: 문자열 정수의 합, Time: 2.67 ms, Memory: 77.2 MB -BaekjoonHub

--- Java/프로그래머스/0/181849. 문자열 정수의 합/README.md
+++ Java/프로그래머스/0/181849. 문자열 정수의 합/README.md
@@ -1,10 +1,10 @@
 # [level 0] 문자열 정수의 합 - 181849 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181849?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181849) 
 
 ### 성능 요약
 
-메모리: 80.7 MB, 시간: 0.81 ms
+메모리: 77.2 MB, 시간: 2.67 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 17일 17:56:18
+2025년 02월 16일 11:44:10
 
 ### 문제 설명
 
--- Java/프로그래머스/0/181849. 문자열 정수의 합/문자열 정수의 합.java
+++ Java/프로그래머스/0/181849. 문자열 정수의 합/문자열 정수의 합.java
@@ -1,13 +1,17 @@
-// import java.util.stream.*;
+import java.util.*;
+import java.util.stream.*;
 
 class Solution {
     public int solution(String str) {
         
-        int answer = 0;
-        for(String v : str.split(""))
-            answer += Integer.parseInt(v);
+//         int answer = 0;
+//         for(String v : str.split(""))
+//             answer += Integer.parseInt(v);
         
-        return answer;
+//         return answer;
+        
+        return Arrays.stream(str.split("")).mapToInt(Integer::parseInt).sum();
+        
         // return Stream.of(str.split("")).mapToInt(Integer::parseInt).sum();
         
     }
